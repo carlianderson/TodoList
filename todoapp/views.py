4e5b155f32232fa2eceee1cc471257a61631acd2
Carli Anderson-Injugu <carlianderson@users.noreply.github.com>
2022-02-24 16:47:58 -0800

    Initial view commit

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def index(request):
+    return render(request, 'todoapp/index.html')
